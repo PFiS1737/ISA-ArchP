83db25a183890814826fcdfdf9e627b47e35228c
PFiS1737 <pfis1737@gmail.com>
2025-12-05 17:49:07 +0800

    feat(cli): allowed to directly output to a file

--- src/cli.rs
+++ src/cli.rs
@@ -1,24 +1,68 @@
+use std::{
+    fmt::Display,
+    fs::File,
+    io::{Write, stdout},
+};
+
+use anyhow::Result;
 use clap::{
     Parser,
-    ValueHint::{DirPath, FilePath},
+    ValueHint::FilePath,
     builder::{Styles, styling::AnsiColor},
 };
 use clap_complete::Shell;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-#[command(arg_required_else_help = true)]
 #[command(styles = get_styles())]
 pub struct Cli {
     /// Print shell auto completions for the specified shell.
-    #[arg(long)]
+    #[arg(long, exclusive = true)]
     pub complete: Option<Shell>,
 
-    #[arg(value_hint = FilePath)]
-    pub src_file: String,
+    /// File path to the source assembly file.
+    #[arg(value_hint = FilePath, required_unless_present = "complete")]
+    pub src_file: Option<String>,
 
-    #[arg(value_hint = DirPath)]
-    pub dist_dir: Option<String>,
+    /// The output file path.
+    #[arg(short, long, value_hint = FilePath, default_value_t = Output::Stdout)]
+    pub output: Output,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Output {
+    Stdout,
+    File(String),
+}
+
+impl Output {
+    pub fn get(&self) -> Result<Box<dyn Write>> {
+        match self {
+            Self::Stdout => Ok(Box::new(stdout())),
+            Self::File(path) => Ok(Box::new(File::create(path)?)),
+        }
+    }
+}
+
+const STDOUT: &str = "<stdout>";
+
+impl Display for Output {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Stdout => write!(f, "{}", STDOUT),
+            Self::File(path) => write!(f, "{}", path),
+        }
+    }
+}
+
+impl From<&str> for Output {
+    fn from(value: &str) -> Self {
+        if value == STDOUT {
+            Self::Stdout
+        } else {
+            Self::File(value.to_owned())
+        }
+    }
 }
 
 fn get_styles() -> Styles {
@@ -30,4 +74,6 @@ fn get_styles() -> Styles {
         .placeholder(AnsiColor::White.on_default())
         .error(AnsiColor::Red.on_default().bold())
         .literal(AnsiColor::Green.on_default())
+        .context(AnsiColor::Cyan.on_default())
+        .context_value(AnsiColor::Magenta.on_default())
 }
--- src/main.rs
+++ src/main.rs
@@ -8,35 +8,41 @@ mod pass1;
 mod pass2;
 mod pseudo_instructions;
 
-use std::{fs::read_to_string, io::stdout};
+use std::{fs::read_to_string, io::stdout, process::ExitCode};
 
+use anyhow::Result;
 use clap::{CommandFactory, Parser};
 use clap_complete::generate;
 
 use crate::{assembler::Assembler, cli::Cli};
 
-fn main() {
+fn main() -> Result<ExitCode> {
     let cli = Cli::parse();
 
     if let Some(shell) = cli.complete {
-        return generate(
+        generate(
             shell,
             &mut Cli::command(),
             env!("CARGO_BIN_NAME"),
             &mut stdout(),
         );
+        return Ok(ExitCode::SUCCESS);
     }
 
-    let source_lines = read_to_string(&cli.src_file)
-        .expect("Failed to read source file")
+    let Some(src_file) = cli.src_file else {
+        unreachable!()
+    };
+
+    let source_lines = read_to_string(src_file)?
         .lines()
         .map(|s| s.to_string())
         .collect();
 
-    for (code, line) in Assembler::new(source_lines)
-        .assemble()
-        .expect("Assembly failed")
-    {
-        println!("0x{:08X} # {}", code, line);
+    let mut out = cli.output.get()?;
+
+    for (code, line) in Assembler::new(source_lines).assemble()? {
+        writeln!(out, "0x{:08X} # {}", code, line)?;
     }
+
+    Ok(ExitCode::SUCCESS)
 }
