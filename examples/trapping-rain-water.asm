8d55962515fa48ed1353793e7008567edb95b373
PFiS1737 <pfis1737@gmail.com>
2025-12-06 23:08:15 +0800

    test: update examples

--- examples/trapping-rain-water.asm
+++ examples/trapping-rain-water.asm
@@ -14,60 +14,63 @@ const a0 r8
 const a1 r9
 const rt r10
 
-li t0 16
-input:
-  sw t1 io 0
-  inc t1
-  dec t0
-  bgt t0 r0 input # r0 = 0
+main:
+  li t0 16
+  input:
+    sw t1 io 0
+    inc t1
+    dec t0
+    bgt t0 r0 input # r0 = 0
 
-# l = 0
-li l 0
-# r = len - 1 = 15
-li r 15
-# lmax = ht[l]
-lw lmax l 0
-# rmax = ht[r]
-lw rmax r 0
+  call solve
 
-# while l < r:
-_while: bge l r _endwhile # {
+  mv io rt
 
-  # if lmax < rmax:
-  bge lmax rmax _else # {
-    # l++
-    inc l
-    # lmax = max(lmax, ht[l])
-    lw a0 l 0
-    mv a1 lmax
-    call max
-    mv lmax rt
-    # water += lmax - ht[l]
-    sub t1 lmax a0
-    add water water t1
+solve:
+  # l = 0
+  li l 0
+  # r = len - 1 = 15
+  li r 15
+  # lmax = ht[l]
+  lw lmax l 0
+  # rmax = ht[r]
+  lw rmax r 0
 
-    jmp _endif
-  # }
+  # while l < r:
+  while: bge l r endwhile
+    # if lmax < rmax:
+    bge lmax rmax else
+      # l++
+      inc l
+      # lmax = max(lmax, ht[l])
+      lw a0 l 0
+      mv a1 lmax
+      call max
+      mv lmax rt
+      # water += lmax - ht[l]
+      sub t1 lmax a0
+      add water water t1
 
-  _else: # {
-    # r--
-    dec r
-    # rmax = max(rmax, ht[r])
-    lw a0 r 0
-    mv a1 rmax
-    call max
-    mv rmax rt
-    # water += rmax - ht[r]
-    sub t1 rmax a0
-    add water water t1
-  # }
- 
-  _endif:
-  jmp _while
-# }
+      jmp endif
 
-_endwhile:
-mv io water
+    else:
+      # r--
+      dec r
+      # rmax = max(rmax, ht[r])
+      lw a0 r 0
+      mv a1 rmax
+      call max
+      mv rmax rt
+      # water += rmax - ht[r]
+      sub t1 rmax a0
+      add water water t1
+
+    endif:
+      jmp while
+
+  endwhile:
+    mv rt water
+    ret
 
 max:
   cmp a0 a1
