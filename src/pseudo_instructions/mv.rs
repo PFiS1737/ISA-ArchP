6aaac603ad9af64bfc9eb648c4839766168a502f
PFiS1737 <pfis1737@gmail.com>
2025-12-06 12:18:26 +0800

    feat(isa)!: changed format of 'mv'

--- src/pseudo_instructions/mv.rs
+++ src/pseudo_instructions/mv.rs
@@ -1,6 +1,6 @@
 use crate::pseudo_instruction;
 
-// mv rs rd  =>  addi rd rs 0
+// mv rd rs  =>  addi rd rs 0
 pseudo_instruction! {
     name: "mv",
     format: [ Reg, Reg ],
@@ -8,8 +8,8 @@ pseudo_instruction! {
         Ok(vec![(
             "addi",
             vec![
-                operands[1].to_string(),
                 operands[0].to_string(),
+                operands[1].to_string(),
                 "0".to_string(),
             ],
         )])
